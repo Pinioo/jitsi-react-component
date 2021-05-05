6c788a606502cdc762b45f47e2a1fc9659127d2a
Pinioo <jakpin1999@gmail.com>
2021-05-05 20:16:07 +0200

    better code now

--- src/VideoConference.js
+++ src/VideoConference.js
@@ -4,43 +4,51 @@ const VideoConference = (props) => {
   const jitsiContainerId = "jitsi-container-id"
   const [jitsi, setJitsi] = React.useState({})
 
-  const initialiseJitsi = async () => {
-    if(!document.getElementById("jitsi")){
-      const script = document.createElement("script")
-      script.src = "https://meet.jit.si/external_api.js"
-      script.async = true
-      script.id = "jitsi"
-      document.body.appendChild(script)
-      script.onload = () => { 
-        const _jitsi = new window.JitsiMeetExternalAPI("meet.jit.si", {
-            roomName: props.roomName,
-            userInfo: {
-              displayName: props.displayName
-            },
-            configOverwrite: {
-              hideConferenceSubject: true,
-              enableNoisyMicDetection: false,
-              prejoinPageEnabled: false
-            },
-            interfaceConfigOverwrite: {
-              TOOLBAR_BUTTONS: [
-                  'microphone', 'camera', 'chat', 'settings'
-              ],
-              TOOLBAR_ALWAYS_VISIBLE: false
-            },
-            parentNode: document.getElementById(jitsiContainerId)
-        })
+  const loadJitsiAPI = () => {
+    let resolver = null;
 
-        setJitsi(_jitsi)
-      }
-    }
+    const onloadPromise = new Promise((resolve) => {
+      resolver = resolve;
+    });
+  
+    const script = document.createElement("script")
+    script.src = "https://meet.jit.si/external_api.js"
+    script.async = true
+    script.onload = () => resolver()
+    document.body.appendChild(script)
+    return onloadPromise
   }
 
-  React.useEffect(() => {
-    return () => jitsi?.dispose?.()
-  }, [jitsi])
+  const initJitsi = async () => {
+    await loadJitsiAPI()
+    setJitsi(createJistiMeet())
+  }
 
-  initialiseJitsi()
+  const createJistiMeet = () => {
+    return new window.JitsiMeetExternalAPI("meet.jit.si", {
+      roomName: props.roomName,
+      userInfo: {
+        displayName: props.displayName
+      },
+      configOverwrite: {
+        hideConferenceSubject: true,
+        enableNoisyMicDetection: false,
+        prejoinPageEnabled: false
+      },
+      interfaceConfigOverwrite: {
+        TOOLBAR_BUTTONS: [
+            'microphone', 'camera', 'chat', 'settings'
+        ],
+        TOOLBAR_ALWAYS_VISIBLE: false
+      },
+      parentNode: document.getElementById(jitsiContainerId)
+    })
+  }
+  
+  React.useEffect(() => {
+    initJitsi()
+    return () => jitsi?.dispose?.()
+  }, [])
 
   return <div id={jitsiContainerId} style={{ height:720, width: "40%" }} />
 }
